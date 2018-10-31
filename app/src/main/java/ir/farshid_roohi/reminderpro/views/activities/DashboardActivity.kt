da527aa200b682896539e9f7360d596956945a81
farshidroohi <farshid.roohi.a@gmail.com>
2018-10-31 22:15:32 +0330

    create activities create animated item view click

--- app/src/main/java/ir/farshid_roohi/reminderpro/views/activities/DashboardActivity.kt
+++ app/src/main/java/ir/farshid_roohi/reminderpro/views/activities/DashboardActivity.kt
@@ -1,9 +1,10 @@
 package ir.farshid_roohi.reminderpro.views.activities
 
+import android.animation.ObjectAnimator
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import ir.farshid_roohi.reminderpro.R
 import ir.farshid_roohi.reminderpro.customViews.CustomItemView
@@ -12,18 +13,23 @@ import ir.farshid_roohi.reminderpro.customViews.CustomItemView
  * Created by Farshid Roohi.
  * ReminderPro | Copyrights 2018.
  */
+
 class DashboardActivity : BaseActivity(), View.OnClickListener {
+
+    lateinit var itemReminderLocation: CustomItemView
+    lateinit var itemNote: CustomItemView
+    lateinit var itemSoundRecorder: CustomItemView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val itemNote = findViewById<CustomItemView>(R.id.item_note)
-        val itemReminderLocation = findViewById<CustomItemView>(R.id.item_reminder_location)
-        val itemSoundRecorder = findViewById<CustomItemView>(R.id.item_sound_recorder)
+        itemReminderLocation = findViewById(R.id.item_reminder_location)
+        itemNote = findViewById(R.id.item_note)
+        itemSoundRecorder = findViewById(R.id.item_sound_recorder)
 
-        var txtHi = findViewById<TextView>(R.id.txt_hi)
-        var imgProfile = findViewById<ImageView>(R.id.img_profile)
+//        val txtHi = findViewById<TextView>(R.id.txt_hi)
+        val imgProfile = findViewById<ImageView>(R.id.img_profile)
 
         itemNote.setOnClickListener(this)
         itemReminderLocation.setOnClickListener(this)
@@ -31,16 +37,56 @@ class DashboardActivity : BaseActivity(), View.OnClickListener {
         imgProfile.setOnClickListener(this)
 
 
+        animatedView(itemReminderLocation, 400)
+        animatedView(itemNote, 600)
+        animatedView(itemSoundRecorder, 800)
+
     }
+
+
+    private fun animatedView(view: View, time: Long) {
+        ObjectAnimator.ofFloat(view, View.TRANSLATION_X, 3000f, 0f).apply {
+            duration = time
+            start()
+        }
+    }
+
+    private fun animatedViewGone(view: View, time: Long) {
+        Thread().run {
+            ObjectAnimator.ofFloat(view, View.TRANSLATION_X, 100f, 3000f).apply {
+                duration = time
+                start()
+            }
+        }.start()
+
+    }
+
 
     override fun onClick(v: View?) {
         when (v!!.id) {
-            R.id.item_note -> Toast.makeText(this, "note", Toast.LENGTH_SHORT).show()
-            R.id.item_reminder_location -> Toast.makeText(this, "reminder location", Toast.LENGTH_SHORT).show()
-            R.id.item_sound_recorder -> Toast.makeText(this, "sound recorder", Toast.LENGTH_SHORT).show()
-            R.id.img_profile -> Toast.makeText(this, "profile", Toast.LENGTH_SHORT).show()
-
+            R.id.item_note -> {
+                animatedViewGone(itemNote, 600)
+                ListNoteActivity.start(this)
+            }
+            R.id.item_reminder_location -> {
+                animatedViewGone(itemReminderLocation, 400)
+                ReminderLocationActivity.start(this)
+            }
+            R.id.item_sound_recorder -> {
+                animatedViewGone(itemSoundRecorder, 800)
+               SoundRecorderActivity.start(this)
+            }
+            R.id.img_profile -> {
+                Toast.makeText(this, "profile", Toast.LENGTH_SHORT).show()
+            }
 
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        animatedView(itemReminderLocation, 400)
+        animatedView(itemNote, 600)
+        animatedView(itemSoundRecorder, 800)
     }
 }
---
+++ app/src/main/java/ir/farshid_roohi/reminderpro/views/activities/ListNoteActivity.kt
@@ -0,0 +1,25 @@
+package ir.farshid_roohi.reminderpro.views.activities
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import ir.farshid_roohi.reminderpro.R
+
+/**
+ * Created by Farshid Roohi.
+ * ReminderPro | Copyrights 2018.
+ */
+class ListNoteActivity : BaseActivity() {
+
+    companion object {
+        fun start(context: Context) {
+            val intent = Intent(context, ListNoteActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_note)
+    }
+}
