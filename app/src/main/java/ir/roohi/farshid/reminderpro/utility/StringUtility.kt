a0be14b6f2378799a62bc5f95af28232a696809e
farshidroohi <farshid.roohi.a@gmail.com>
2018-12-04 00:33:19 +0330

    add counter and play time

--- app/src/main/java/ir/roohi/farshid/reminderpro/utility/StringUtility.kt
+++ app/src/main/java/ir/roohi/farshid/reminderpro/utility/StringUtility.kt
@@ -1,6 +1,9 @@
 package ir.roohi.farshid.reminderpro.utility
 
+import android.content.Context
+import ir.roohi.farshid.reminderpro.R
 import java.lang.StringBuilder
+import java.text.DecimalFormat
 import java.util.*
 
 /**
@@ -16,7 +19,52 @@ fun randomName(len: Int = 20): String {
         stringBuilder.append(character[Random().nextInt(character.length)])
         i++
     }
-
-
     return stringBuilder.toString()
+}
+
+fun convertToTime(counter: Float): String {
+    var counter = counter
+
+    val ms = (counter % 1000).toInt() / 10
+    counter = (counter.toInt() / 1000).toFloat()
+    val hours = ms / (1000 * 60 * 60)
+    val min = counter.toInt() / 60
+    val sec = counter.toInt() - min * 60
+    var hourPrefix = ""
+    var minPrefix = ""
+    var secPrefix = ""
+    var msPrefix = ""
+
+    if (hours < 10)
+        hourPrefix = "0"
+    if (min < 10)
+        minPrefix = "0"
+    if (sec < 10)
+        secPrefix = "0"
+    if (ms < 10)
+        msPrefix = "0"
+
+    return "$hourPrefix$hours : $minPrefix$min : $secPrefix$sec : $msPrefix$ms"
+}
+
+fun formatFileSize(size: Long): String {
+    val hrSize: String
+
+    val b = size.toDouble()
+    val k = size / 1024.0
+    val m = size / 1024.0 / 1024.0
+    val g = size / 1024.0 / 1024.0 / 1024.0
+    val t = size / 1024.0 / 1024.0 / 1024.0 / 1024.0
+
+    val dec = DecimalFormat("0.00")
+
+    hrSize = when {
+        t > 1 -> dec.format(t) + ("  TB")
+        g > 1 -> dec.format(g) + ("  GB")
+        m > 1 -> dec.format(m) + ("  MG")
+        k > 1 -> dec.format(k) + ("  KB")
+        else -> dec.format(b) + ("  B")
+    }
+
+    return hrSize
 }
--- app/src/main/java/ir/roohi/farshid/reminderpro/views/activities/RecordSoundActivity.kt
+++ app/src/main/java/ir/roohi/farshid/reminderpro/views/activities/RecordSoundActivity.kt
@@ -11,10 +11,13 @@ import android.view.View
 import android.widget.Toast
 import ir.roohi.farshid.reminderpro.R
 import ir.roohi.farshid.reminderpro.customViews.AlertDialog
+import ir.roohi.farshid.reminderpro.utility.convertToTime
+import ir.roohi.farshid.reminderpro.utility.formatFileSize
 import ir.roohi.farshid.reminderpro.utility.randomName
 import kotlinx.android.synthetic.main.activity_record_sound.*
 import java.io.File
 import java.io.IOException
+import java.lang.Exception
 
 
 /**
@@ -23,10 +26,11 @@ import java.io.IOException
  */
 class RecordSoundActivity : BaseActivity(), View.OnClickListener, BaseActivity.OnPermissionRequestListener {
 
-    private var flagRecording = false
+    private var oncePlay = true
     private lateinit var recorder: MediaRecorder
-    private lateinit var player: MediaPlayer
+    private var player = MediaPlayer()
     private lateinit var filePath: String
+    private var status: Status = Status.RECORD
 
 
     private val permissions = arrayOf(Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE)
@@ -45,15 +49,15 @@ class RecordSoundActivity : BaseActivity(), View.OnClickListener, BaseActivity.O
         setContentView(R.layout.activity_record_sound, R.color.color_gradient_dark_two)
 
 
-        // TODO : fix UI record & play voice
-
         this.player = MediaPlayer()
 
 
-        layoutRecordState.setOnClickListener(this)
+        fabRecord.setOnClickListener(this)
         imgBack.setOnClickListener(this)
         imgShare.setOnClickListener(this)
-        btnPlay.setOnClickListener(this)
+        txtSave.setOnClickListener(this)
+        txtDelete.setOnClickListener(this)
+        txtPlay.setOnClickListener(this)
 
         requestPermission(permissions, this)
 
@@ -61,49 +65,167 @@ class RecordSoundActivity : BaseActivity(), View.OnClickListener, BaseActivity.O
 
     override fun onClick(v: View?) {
         when (v!!.id) {
-            R.id.layoutRecordState -> {
-                this.flagRecording = !this.flagRecording
+            R.id.fabRecord -> {
                 handledVoiceRecording()
-
-                if (flagRecording) {
-                    this.recorder.start()
-                    return
-                }
-                this.recorder.stop()
-                this.recorder.release()
             }
             R.id.imgBack -> finish()
             R.id.imgShare -> {
                 customProgressCircle.stopAnimated()
                 Toast.makeText(this, "share", Toast.LENGTH_SHORT).show()
+
             }
-            R.id.btnPlay -> {
-                if (!this.flagRecording && File(filePath).exists() && !player.isPlaying) {
+            R.id.txtDelete -> {
+                counterPlay = 1000000000
+                status = Status.RECORD
+                fabRecord.show()
+                txtPlay.visibility = View.GONE
+                File(filePath).delete()
+                oncePlay = true
+                this.player = MediaPlayer()
+                this.fabRecord.setImageResource(R.drawable.ic_keyboard_voice)
+                this.customProgressCircle.stopAnimated()
+                this.txtTime.text = getString(R.string.zero_size)
+                this.txtSize.text = getString(R.string.zero_size)
+            }
+            R.id.txtSave -> {
+                showMsg(getString(R.string.save))
+                finish()
+            }
+            R.id.txtPlay -> {
+                if (this.player.isPlaying) {
+                    player.stop()
+                    this.customProgressCircle.stopAnimated()
+                    this.fabRecord.setImageResource(R.drawable.ic_play)
+                    return
+                }
+                if (!File(filePath).exists()) {
+                    showMsg("problem play voice")
+                    return
+                }
+                if (oncePlay) {
                     this.player.setDataSource(this.filePath)
                     this.player.prepare()
-                    this.player.start()
+                }
+                this.player.start()
+                oncePlay = false
+                counterThreadPlay()
+
+                this.fabRecord.setImageResource(R.drawable.ic_stop)
+                this.customProgressCircle.startAnimated()
+            }
+
+
+        }
+    }
+
+    var counter = 0
+    var counterPlay = 0
+    private fun counterThread() {
+        val thread = object : Thread() {
+            override fun run() {
+                while (status == Status.STOP) {
+                    try {
+                        Thread.sleep(100)
+                        counter += 100
+                        runOnUiThread {
+                            txtTime.text = convertToTime(counter.toFloat()) + ""
+                            txtSize.text = formatFileSize(File(filePath).length())
+
+                        }
+
+                    } catch (e: InterruptedException) {
+                        e.printStackTrace()
+                    }
+
                 }
             }
         }
+        thread.start()
+    }
+
+    private fun threadSize() {
+        val file = File(filePath)
+        val thread = object : Thread() {
+            override fun run() {
+                super.run()
+                while (status == Status.STOP) {
+                    try {
+                        Thread.sleep(1500)
+                        Log.i("TAGAA", " size : ${file.length() /1024}")
+
+                        runOnUiThread {
+                            txtSize.text = formatFileSize(file.length())
+
+                        }
+
+                    } catch (e: InterruptedException) {
+                        e.printStackTrace()
+                    }
+
+                }
+            }
+        }
+        thread.start()
+    }
+
+    private fun counterThreadPlay() {
+        counterPlay = 0
+
+        val thread = object : Thread() {
+            override fun run() {
+                while (counterPlay <= counter) {
+                    try {
+                        Thread.sleep(100)
+                        counterPlay += 100
+
+                        runOnUiThread { txtTime.text = convertToTime(counterPlay.toFloat()) + "" }
+
+                    } catch (e: InterruptedException) {
+                        e.printStackTrace()
+                    }
+
+                }
+                if (counterPlay >= counter) {
+                    runOnUiThread {
+                        txtTime.text = convertToTime(counter.toFloat()) + ""
+                    }
+                }
+            }
+        }
+        thread.start()
+
     }
 
 
     private fun handledVoiceRecording() {
-        if (this.flagRecording) {
-            this.txtTitleStatus.text = getString(R.string.stop_recording)
-            this.imgStatus.setImageResource(R.drawable.ic_stop)
-            this.customProgressCircle.startAnimated()
-            return
+        when (status) {
+            Status.RECORD -> {
+                prepare()
+                this.fabRecord.setImageResource(R.drawable.ic_stop)
+                this.customProgressCircle.startAnimated()
+                this.recorder.start()
+                status = Status.STOP
+                counterThread()
+                threadSize()
+            }
+            Status.STOP -> {
+                this.fabRecord.setImageResource(R.drawable.ic_play)
+                this.customProgressCircle.stopAnimated()
+                counterPlay = 1000000000
+                this.recorder.stop()
+                this.recorder.release()
+                status = Status.PLAY
+                fabRecord.hide()
+                txtPlay.visibility = View.VISIBLE
+            }
+            else -> {
+            }
         }
-        this.txtTitleStatus.text = getString(R.string.start_recording)
-        this.imgStatus.setImageResource(R.drawable.ic_play)
-        this.customProgressCircle.stopAnimated()
 
     }
 
     private fun prepare() {
         this.filePath = String.format("%s%s%s%s", resourceApp!!.getDirSoundSave(), "reminderPro-", randomName(), ".3gp")
-        Log.i("TAG_A", "${File(resourceApp!!.getDirSoundSave()).mkdirs()}")
         this.recorder = MediaRecorder()
         recorder.setAudioSource(MediaRecorder.AudioSource.MIC)
         recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
@@ -135,6 +257,12 @@ class RecordSoundActivity : BaseActivity(), View.OnClickListener, BaseActivity.O
             finish()
         })
         alertBuilder.build().show()
+    }
+
+    enum class Status {
+        RECORD,
+        PLAY,
+        STOP
     }
 
 
