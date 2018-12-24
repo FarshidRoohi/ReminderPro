4d0166dca8241145d46adad113d4ef4c94c390db
farshidroohi <farshid.roohi.a@gmail.com>
2018-12-24 12:03:50 +0330

    convert java to kotlin class 100% kotlin in project

---
+++ app/src/main/java/ir/roohi/farshid/reminderpro/database/DateConverter.kt
@@ -0,0 +1,23 @@
+package ir.roohi.farshid.reminderpro.database
+
+
+import android.arch.persistence.room.TypeConverter
+
+import java.util.*
+
+/**
+ * Created by Farshid Roohi.
+ * ReminderPro | Copyrights 2018.
+ */
+class DateConverter {
+
+    @TypeConverter
+    fun toDate(time: Long?): Date? {
+        return if (time == null) null else Date(time)
+    }
+
+    @TypeConverter
+    fun toTimesTemp(date: Date?): Long? {
+        return date?.time
+    }
+}
---
+++ app/src/main/java/ir/roohi/farshid/reminderpro/repository/NoteRepository.kt
@@ -0,0 +1,57 @@
+package ir.roohi.farshid.reminderpro.repository
+
+import android.arch.lifecycle.LiveData
+import android.content.Context
+import ir.roohi.farshid.reminderpro.database.AppDatabase
+import ir.roohi.farshid.reminderpro.model.NoteEntity
+import java.util.concurrent.Executors
+
+/**
+ * Created by Farshid Roohi.
+ * ReminderPro | Copyrights 2018.
+ */
+class NoteRepository private constructor(context: Context) {
+    var notes: LiveData<List<NoteEntity>>
+    private val database: AppDatabase? = AppDatabase.getInstance(context)
+    private val executor = Executors.newSingleThreadExecutor()
+
+
+    private val allNotes: LiveData<List<NoteEntity>>
+        get() = this.database!!.noteDao().all
+
+    init {
+        this.notes = allNotes
+    }
+
+    fun deleteAllNotes() {
+        executor.execute { database!!.noteDao().deleteAll() }
+    }
+
+    fun getNoteById(idNote: Int): NoteEntity {
+        return database!!.noteDao().getNotesById(idNote)
+    }
+
+    fun insertNote(note: NoteEntity) {
+        executor.execute { database!!.noteDao().insertNote(note) }
+    }
+
+    fun updateNote(note: NoteEntity) {
+        executor.execute { database!!.noteDao().updateNote(note.id, note.date!!, note.title!!, note.text!!) }
+    }
+
+    fun deleteNote(noteEntity: NoteEntity) {
+        executor.execute { database!!.noteDao().deleteNote(noteEntity) }
+    }
+
+    companion object {
+
+        private var ourInstance: NoteRepository? = null
+
+        fun getInstance(context: Context): NoteRepository {
+            if (ourInstance == null) {
+                ourInstance = NoteRepository(context)
+            }
+            return ourInstance!!
+        }
+    }
+}
