2611af1597b943bde4a73ce1a2fb3ca64c01c164
farshidroohi <farshid.roohi.a@gmail.com>
2019-01-04 20:49:13 +0330

    remove zoom button add my location fab button

--- app/src/main/java/ir/roohi/farshid/reminderpro/views/activities/MapActivity.kt
+++ app/src/main/java/ir/roohi/farshid/reminderpro/views/activities/MapActivity.kt
@@ -2,13 +2,13 @@ package ir.roohi.farshid.reminderpro.views.activities
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.preference.PreferenceManager
 import ir.roohi.farshid.reminderpro.R
 import kotlinx.android.synthetic.main.activity_map.*
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
@@ -18,6 +18,8 @@ import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
  * ReminderPro | Copyrights 1/1/19.
  */
 class MapActivity : BaseActivity() {
+
+    private var mMyLocationOverlay: MyLocationNewOverlay? = null
 
     companion object {
         fun start(context: Context) {
@@ -31,32 +33,14 @@ class MapActivity : BaseActivity() {
         setContentView(R.layout.activity_map)
 
 
-        mapView.setMultiTouchControls(true)
         mapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE)
         mapView.setMultiTouchControls(true)
         mapView.setHasTransientState(true)
-        mapView.controller.setZoom(5)
-
+        mapView.controller.setZoom(15)
         mapView.dispatchSetSelected(true)
-
-
-        val mMyLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(this), mapView)
-
-        mMyLocationOverlay.setPersonIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_person_pin))
-
-
-        val mapController = mapView.controller
-
-        mMyLocationOverlay.enableMyLocation()
-        mMyLocationOverlay.enableFollowLocation()
-        mMyLocationOverlay.isDrawAccuracyEnabled = true
-        mMyLocationOverlay.runOnFirstFix {
-            runOnUiThread {
-                mapController.animateTo(mMyLocationOverlay.myLocation)
-                mapController.setZoom(18)
-            }
-        }
-        mapView.overlays.add(mMyLocationOverlay)
+        mapView.setBuiltInZoomControls(false)
+        fabMyLocation.setOnClickListener { myLocation() }
+        myLocation()
 
 
 //        val compassOverlay = CompassOverlay(this, InternalCompassOrientationProvider(this), mapView)
@@ -82,6 +66,29 @@ class MapActivity : BaseActivity() {
 //        mOverlay.setFocusItemsOnTap(true)
 //        mapView.overlays.add(mOverlay)
 
+
+    }
+
+
+    private fun myLocation() {
+        val mapController = mapView.controller
+
+        if (mMyLocationOverlay == null) {
+            mMyLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(this), mapView)
+//            mMyLocationOverlay!!.setPersonIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_person_pin))
+            val gp = GeoPoint(35.6980012, 51.3337879)
+            mapController.setCenter(gp)
+        }
+        mMyLocationOverlay!!.enableMyLocation()
+        mMyLocationOverlay!!.enableFollowLocation()
+        mMyLocationOverlay!!.isDrawAccuracyEnabled = true
+        mMyLocationOverlay!!.runOnFirstFix {
+            runOnUiThread {
+                mapController.animateTo(mMyLocationOverlay!!.myLocation)
+                mapController.setZoom(18)
+            }
+        }
+        mapView.overlays.add(mMyLocationOverlay)
 
     }
 
