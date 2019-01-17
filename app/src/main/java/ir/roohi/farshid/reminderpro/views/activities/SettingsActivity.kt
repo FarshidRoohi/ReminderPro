de82e232cf0cc2c20ab4dc7a24f6cfc7b4327390
farshid.roohi.a@gmail.com <farshid.roohi.a@gmail.com>
2019-01-17 20:07:49 +0330

    add two language setting activity

--- app/src/main/java/ir/roohi/farshid/reminderpro/views/activities/SettingsActivity.kt
+++ app/src/main/java/ir/roohi/farshid/reminderpro/views/activities/SettingsActivity.kt
@@ -2,14 +2,21 @@ package ir.roohi.farshid.reminderpro.views.activities
 
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
 import ir.roohi.farshid.reminderpro.R
+import kotlinx.android.synthetic.main.activity_settings.*
+import java.util.*
 
 /**
  * Created by Farshid Roohi.
  * ReminderPro | Copyrights 1/17/19.
  */
 class SettingsActivity : BaseActivity() {
+
+    private var sharedPreferences: SharedPreferences? = null
+    private var currentLanguage: String? = null
 
     companion object {
         fun start(context: Context) {
@@ -18,8 +25,33 @@ class SettingsActivity : BaseActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        this.sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        this.currentLanguage = sharedPreferences!!.getString("LANGUAGE", "EN")
+        setLocale(this.currentLanguage!!)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        toolbar.getLeftImageView().setOnClickListener { finish() }
+
+        btnChangeLanguage.text =
+                if (this.currentLanguage == "FA") getString(R.string.change_to_en) else getString(R.string.change_to_fa)
+
+        btnChangeLanguage.setOnClickListener {
+            currentLanguage = if (currentLanguage!! == "EN") "FA" else "EN"
+            sharedPreferences!!.edit().putString("LANGUAGE", currentLanguage!!).apply()
+            setLocale(this.currentLanguage!!)
+            recreate()
+        }
+
+    }
+
+    private fun setLocale(lang: String) {
+        val locale = Locale(lang)
+        val res = resources
+        val displayMatris = res.displayMetrics
+        val configuration = res.configuration
+        configuration.locale = locale
+        res.updateConfiguration(configuration, displayMatris)
 
     }
 }
